612b3571eae69d9bd677a5189d2a4ebb6759ccc8
Laihiujin <lixiaoran0817@outlook.com>
2023-03-14 06:58:21 +0800

    Site updated: 2023-03-14 06:58:21

---
+++ dist/music.js
@@ -0,0 +1,116 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    pjax: true,
+    audio: [
+        {
+            name: 'Stranger Things',
+            artist: 'Kyle Dixon & Michael Stein',
+            url: 'https://link.hhtjim.com/163/425684058.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: "Everywhere We Go",
+            artist: '陈冠希/MC仁/厨房仔/应采儿',
+            url: 'https://link.hhtjim.com/163/65739.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'Running Up That Hill',
+            artist: 'Baltic House Orchestra',
+            url: 'https://link.hhtjim.com/163/1959556810.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'Fuerteventura',
+            artist: 'Russian Red',
+            url: 'https://link.hhtjim.com/163/560513311.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'Call Your Name x Call of Silence',
+            artist: 'Kyle Dixon & Michael Stein',
+            url: 'https://link.hhtjim.com/163/1826307498.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'THE REAL FOLK BLUES',
+            artist: '山根麻衣',
+            url: 'https://link.hhtjim.com/163/591379.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'See You Space Cowboys Not Final Mix Mountain Root',
+            artist: '山根麻衣',
+            url: 'https://link.hhtjim.com/163/590883.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: 'Gotta Knock a Little Harder',
+            artist: '山根麻衣',
+            url: 'https://link.hhtjim.com/163/589968.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: '风的颜色',
+            artist: 'NINEONE',
+            url: 'https://link.hhtjim.com/163/1355896807.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: '几点',
+            artist: '陈冠希/厨房仔/MC HotDog 热狗/李灿森',
+            url: 'https://link.hhtjim.com/163/1379743775.mp3',
+            cover: 'img/6.PNG',
+        },
+        {
+            name: '11',
+            artist: '队长/黄礼格',
+            url: 'https://link.hhtjim.com/163/1907766514.mp3',
+            cover: 'img/6.PNG',
+        },{
+            name: '小事',
+            artist: '白举纲',
+            url: 'https://link.hhtjim.com/163/1411542710.mp3',
+            cover: 'img/6.PNG',
+        },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+        // {
+        //     name: '',
+        //     artist: '',
+        //     url: '',
+        //     cover: 'img/6.png',
+        // },
+
+    ]
+});
